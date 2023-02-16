c5266864e61c43a4a5844d3b00a4024e70fc1379
Romain BOURGOGNE <r.bourgogne@student.isartdigital.com>
2023-02-16 09:54:34 +0100

    Initial commit

---
+++ UFAFAC/Main.cpp
@@ -0,0 +1,11 @@
+#include "Main.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+void main()
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	UFAFAC::Main form;
+	Application::Run(% form);
+}
