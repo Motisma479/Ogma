7d43737fa59b9174e24b31249d2b44e9eadd1d4b
Motisma479 <mat020930@gmail.com>
2023-02-16 11:10:40 +0100

    search/main menu started

--- UFAFAC/Main.h
+++ UFAFAC/Main.h
@@ -34,12 +34,29 @@ namespace UFAFAC {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::MenuStrip^ menuStrip1;
+	private: System::Windows::Forms::ToolStripMenuItem^ quiterToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ exploreToolStripMenuItem;
+	private: System::Windows::Forms::ToolStripMenuItem^ aideToolStripMenuItem;
+	private: System::Windows::Forms::TextBox^ textBox1;
+
+
+
+
+
+
+
+
+	private: System::ComponentModel::IContainer^ components;
+	protected:
+
+
 
 	private:
 		/// <summary>
 		/// Variable nécessaire au concepteur.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -48,12 +65,78 @@ namespace UFAFAC {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"Main";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->menuStrip1 = (gcnew System::Windows::Forms::MenuStrip());
+			this->quiterToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->exploreToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->aideToolStripMenuItem = (gcnew System::Windows::Forms::ToolStripMenuItem());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->menuStrip1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this->menuStrip1->ImageScalingSize = System::Drawing::Size(20, 20);
+			this->menuStrip1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(3) {
+				this->quiterToolStripMenuItem,
+					this->exploreToolStripMenuItem, this->aideToolStripMenuItem
+			});
+			this->menuStrip1->Location = System::Drawing::Point(0, 0);
+			this->menuStrip1->Name = L"menuStrip1";
+			this->menuStrip1->Size = System::Drawing::Size(1882, 30);
+			this->menuStrip1->TabIndex = 0;
+			this->menuStrip1->Text = L"menuStrip1";
+			// 
+			// quiterToolStripMenuItem
+			// 
+			this->quiterToolStripMenuItem->Name = L"quiterToolStripMenuItem";
+			this->quiterToolStripMenuItem->Size = System::Drawing::Size(69, 26);
+			this->quiterToolStripMenuItem->Text = L"Quitter";
+			// 
+			// exploreToolStripMenuItem
+			// 
+			this->exploreToolStripMenuItem->Name = L"exploreToolStripMenuItem";
+			this->exploreToolStripMenuItem->Size = System::Drawing::Size(73, 26);
+			this->exploreToolStripMenuItem->Text = L"Explore";
+			// 
+			// aideToolStripMenuItem
+			// 
+			this->aideToolStripMenuItem->Name = L"aideToolStripMenuItem";
+			this->aideToolStripMenuItem->Size = System::Drawing::Size(54, 26);
+			this->aideToolStripMenuItem->Text = L"Aide";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Anchor = System::Windows::Forms::AnchorStyles::None;
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->textBox1->Location = System::Drawing::Point(560, 370);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(560, 36);
+			this->textBox1->TabIndex = 1;
+			this->textBox1->TextChanged += gcnew System::EventHandler(this, &Main::textBox1_TextChanged);
+			// 
+			// Main
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1882, 1033);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->menuStrip1);
+			this->MainMenuStrip = this->menuStrip1;
+			this->Name = L"Main";
+			this->Text = L"Main";
+			this->menuStrip1->ResumeLayout(false);
+			this->menuStrip1->PerformLayout();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
 		}
 #pragma endregion
-	};
+	private: System::Void feurToolStripMenuItem_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void richTextBox1_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+}
+};
 }
