50989356cb924871e4bcfbf0064d45781a3bd687
Romain BOURGOGNE <r.bourgogne@student.isartdigital.com>
2023-02-23 10:32:41 +0100

    Added Utils file, Search by files by string, Convert system string to std::string

--- UFAFAC/Sources/Main.cpp
+++ UFAFAC/Sources/Main.cpp
@@ -9,3 +9,17 @@ void main()
 	UFAFAC::Main form;
 	Application::Run(% form);
 }
+
+System::Void UFAFAC::Main::textBox1_TextChanged(System::Object^ sender, System::EventArgs^ e)
+{
+	TextBox^ value = ((TextBox^)sender);
+	System::String^ text = (*value).Text;
+	System::Runtime::InteropServices::Marshal::StringToCoTaskMemUni(text);
+
+	std::vector<std::string> tempDatas;
+	tempDatas.push_back("Test1");
+	tempDatas.push_back("Test2");
+	tempDatas.push_back("amogus");
+	tempDatas.push_back("fauygw");
+	auto filename = Utils::GetFilesByName(Utils::SystemStringToStdString(text), tempDatas);
+}
