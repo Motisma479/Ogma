d243ae7b17551fd8c8c64b733f356f1236f3b08c
m.robion <m.robion@student.isartdigital.com>
2024-01-18 10:08:46 +0100

    [add]ImGui added use demo for now

--- OGMA/Sources/main.cpp
+++ OGMA/Sources/main.cpp
@@ -1,6 +1,16 @@
 #include<iostream>
 
+#include <IMGUI/imgui.h>
+#include <IMGUI/backends/imgui_impl_glfw.h>
+#include <IMGUI/backends/imgui_impl_opengl3.h>
 #include <GLFW/glfw3.h>
+#include <cmath>
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+}
 
 int main(void)
 {
@@ -20,18 +30,38 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    //glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
+
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
-
-        /* Poll for and process events */
         glfwPollEvents();
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+
+
+        ImGui::ShowDemoWindow();
+
+        ImGui::Render();
+
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
     }
 
     glfwTerminate();
